75a9fe79c578acf4b7f3867972c5409e6c1daabd
natsu2597 <atulrishi1997@gmail.com>
2026-08-27 21:11:23 +0530

    users serializer urls and views created

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import User
 
-class UserSerializers(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
@@ -47,6 +47,6 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         user.set_password(password)
 
-        user.save
+        user.save()
 
         return user
