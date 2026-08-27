6a21dda83db24af4f5d5acc57e5fcaeffe92ab01
natsu2597 <atulrishi1997@gmail.com>
2026-08-27 22:35:22 +0530

    login serializer, views & urls made and configured

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.password_validation import validate_password
 
+from django.contrib.auth import authenticate
+
 from rest_framework import serializers
 from .models import User
 
@@ -72,3 +74,34 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+   username = serializers.CharField()
+   password = serializers.CharField(
+       write_only=True
+   )
+
+   def validate(self,attrs):
+       username = attrs.get("username")
+       password = attrs.get("password")
+
+       user = authenticate(
+           username=username,
+           password=password
+       ) 
+
+       if user is None:
+           raise serializers.ValidationError({
+               "username" : "User does not exist",
+               "password" : "Password is incorrect"
+           })
+
+       if not user.is_active:
+           raise serializers.ValidationError({
+               "username" : "User is not active"
+           })
+
+       attrs["user"] = user
+
+       return attrs
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegisterSerializer, UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
 
 # Create your views here.
 
@@ -29,4 +29,31 @@ class RegisterView(APIView):
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class LoginView(APIView):
+    permission_classes= [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(
+            data=request.data
+            )
+
+        serializer.is_valid(
+            raise_exception=True
+        )
+
+        user = serializer.validated_data["user"]
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "messgage" : "Login successful",
+                "user" : UserSerializer(user).data,
+                "access" : str(refresh.access_token),
+                "refresh" : str(refresh)
+            },
+            status=status.HTTP_200_OK
         )
