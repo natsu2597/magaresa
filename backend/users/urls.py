d2290d0e6f794c03c8e579e6b8c9db956786f20b
natsu2597 <atulrishi1997@gmail.com>
2026-08-28 11:36:39 +0530

    profile view created and urls configured

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from .views import RegisterView, LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import RegisterView, LoginView, ProfileView
 
 urlpatterns = [
     path("register/", 
@@ -10,4 +12,14 @@ urlpatterns = [
     path("login/",
           LoginView.as_view(),
             name="login"),
+
+    path("profile/",
+            ProfileView.as_view(),
+            name="profile"
+         ),
+
+    path("token/refresh/",
+            TokenRefreshView.as_view(),
+            name="token-refresh"
+         )     
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -56,4 +56,17 @@ class LoginView(APIView):
                 "refresh" : str(refresh)
             },
             status=status.HTTP_200_OK
+        )
+
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK,
         )
